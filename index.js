a441af4e0317955d3479ea20f7619e2e60275967
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-09-30 17:30:14 +0200

    added express / morgan

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+morgan = require('morgan');
+const app = express();
+
+
+
+let topMovies = [{
+    title: 'Heirs of the Night',
+    author: 'D. Van Rooijen'
+  },
+  {
+    title: 'Memoirs of an Invisible Man',
+    author: 'John Carpenter'
+  },
+  {
+    title: 'Van Helsing',
+    author: 'Stephen Sommers'
+  },
+  {
+    title: 'Inception',
+    author: 'Christopher Nolan'
+  },
+  {
+    title: 'Interstellar',
+    author: 'Christopher Nolan'
+  },
+  {
+    title: 'Joker',
+    author: 'Todd Phillips'
+  },
+  {
+    title: 'Django Unchained',
+    author: 'Quentin Tarantino'
+  },
+  {
+    title: 'A Discovery of Witches',
+    author: 'Farren Blackburn'
+  },
+  {
+    title: 'Carnival Row',
+    author: 'Thor Freudenthal'
+  },
+  {
+    title: 'Altered Carbon',
+    author: 'Uta Briesewitz'
+  }
+];
+
+//create an Express GET route located at the endpoint “/movies” that returns
+//a JSON object containing data about your top 10 movies.
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// Create another GET route located at the endpoint “/” that returns a default textual response of your choosing.
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Movie Paradise!');
+});
+
+//express.static
+app.use('/documentation.html', express.static('public'));
+
+//Use the Morgan middleware library to log all requests (instead of using the fs module to write to a text file).
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Movie Paradise!');
+});
+
+
+
+//error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+  console.log('This is an error');
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
