42a7508559edf9aec063cff2cf43abe2a9a38c99
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-11-03 08:46:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,22 +21,23 @@ const Users = Models.User;
 
 
 //cors
-app.use(cors());
+//app.use(cors());
 
 /* ALLOW CERTAIN DOMAIN ONLY */
 
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ['http://localhost:1234', 'https://i.ibb.co'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // middleware
 app.use(bodyParser.json());
